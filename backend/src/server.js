683bee2c27d0ac9088c7d2b95b654a353c6f2347
akku213 <akankshashrivastav2103@gmail.com>
2026-02-24 15:28:07 +0530

    Serve frontend from backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,16 @@
-import express from 'express';
+import path from "path";
+import express from "express";
 import { ENV } from './lib/env.js';
 import { connectDB } from './lib/db.js';
 import cors from 'cors';
 import {serve} from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js"
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
 
 app.use(express.json());// Middleware to parse JSON bodies
 app.use(cors({origin:ENV.CLIENT_URL, credentials:true})); // Enable CORS for the specified client URL with credentials support
